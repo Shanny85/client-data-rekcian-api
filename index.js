0c8dba9e3977896312bda0d7fc5b7e0d66a6fb29
Shanon Naicker <smokeofwar@gmail.com>
2024-10-20 14:38:04 +0200

    removed department stuff as no longer needed

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+{/*update routes into server as created*/}
 import authRouter from './routes/auth.js';
-import departmentRouter from './routes/department.js';
 import employeeRouter from './routes/employee.js';
 import connectionToDatabase from "./db/db.js";
 
-// Load environment variables
-dotenv.config();
-
-// Connect to the database
-connectionToDatabase();
-
-// Create an Express app
+connectionToDatabase().then(r => {})
 const app = express();
-
-// CORS configuration
-
-// Apply CORS middleware
-app.use(cors({
-    origin: "https://client-data-rekcian-frontend.vercel.app"
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/public', express.static('public'));
 app.use(express.static('public/uploads'));
 app.use('/api/auth', authRouter);
-app.use('/api/department', departmentRouter);
 app.use('/api/employee', employeeRouter);
 
 // Use a default port if not specified
